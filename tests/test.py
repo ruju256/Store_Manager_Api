aa62ae2271249d3eb0cfc1b085adb510a6e887f0
ruju256 <ezramahlon@gmail.com>
2018-10-18 13:56:12 +0300

    adding tests

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,21 @@
-import unittest 
 from app.app import app
+import unittest 
 
 
 class StoreEndpoints(unittest.TestCase):
 
-    def test_if_products_endpoit_exists(self):
-        response = self.app.get('/api/v1/products', redirect_resource = True)
+    def setUp(self):
+        self.client = app.test_client(self)
+    
+    def tearDown(self):
+        pass
+    
+    def test_invalid_route(self):
+        response = self.client.get('/api/products')
+        self.assertTrue(response.status_code, 406)
+
+    def test_if_products_endpoint_exists(self):
+        response = self.client.get('/api/v1/products', redirect_resource = True)
         return (response.status, 200)
 
     

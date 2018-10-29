fbf4bf5ec34fe261133f586a967ae12ef3b8c36c
ruju256 <ezramahlon@gmail.com>
2018-10-29 18:20:56 +0300

    adding authentication to all routes. also installed flask_jwt_extended for authentication

--- app.py
+++ app.py
@@ -1,91 +1,72 @@
 import os
 import sys
 from flask import Flask, jsonify, request
+from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity)
 
 
-#creating a list of dictionaries for all product details
 app = Flask(__name__)
 app.secret_key = "#StoreManagerAPIKey"
 
+app.config['JWT_SECRET_KEY'] = '@andela256'
+jwt = JWTManager(app)
+
+#creating a list of dictionaries for all product details
 products = [
-    {
-        'id' : 1,
-        'product_name' : 'Bread',
-        'manufacture_date' : '2018-11-02',
-        'expiry_date' : '2019-01-10'
-    },
-    {
-        'id' : 2,
-        'product_name' : 'Sugar',
-        'manufacture_date' : '2018-05-12',
-        'expiry_date' : '2019-01-06'
-    },
-    {
-        'id' : 3,
-        'product_name' : 'Rice',
-        'manufacture_date' : '2018-06-02',
-        'expiry_date' : '2018-10-13'
-    },
-    {
-        'id' : 4,
-        'product_name' : 'soda',
-        'manufacture_date' : '2018-03-20',
-        'expiry_date' : '2019-01-01'
-    }
+{}
 ]
 
 #creating a list of dictionaries for all sales records
 
 sales = [
-    {
-        'id' : 1,
-        'product_sold' : 'Rice',
-        'quantity' : '3 kgs',
-        'unit_cost' : '3500',
-        'total_cost' : '10500',
-        'attendant' : 'john'
-    },
-    {
-        'id' : 2,
-        'product_sold' : 'Bread',
-        'quantity' : '1 loaf',
-        'unit_cost' : '4500',
-        'total_cost' : '4500',
-        'attendant' : 'john'
-    },
-    {
-        'id' : 3,
-        'product_sold' : 'soda',
-        'quantity' : '2 crates',
-        'unit_cost' : '23000',
-        'total_cost' : '46000',
-        'attendant' : 'mary'
-    },
+    {}
 ]
 
+@app.route("/api/v1/login", methods = ['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    if not email:
+        return jsonify({"msg":"Email can not be empty"}), 400
+    if not password:
+        return jsonify({"msg":"Password can not be empty"}), 400
+
+    if email != 'ezramahlon@gmail.com' or password !='admin123':
+        return jsonify({"msg":"Invalid Email or Password"}), 400
+
+    access_token = create_access_token(identity = email)
+    return jsonify(access_token=access_token),200
+
 @app.route("/api/v1/products", methods = ['GET'])
+@jwt_required
 def get_products():
     return jsonify({'products' : products})
 
 
 @app.route("/api/v1/products/<int:id>", methods = ['GET'])
+@jwt_required
 def get_single_product_details(id):
     product_details = [product for product in products if product['id'] == id]
     return jsonify({'product':product_details[0]})
     
 
 @app.route("/api/v1/sales", methods = ['GET'])
+@jwt_required
 def get_sales_records():
     return jsonify({'sales' : sales})
 
 
 @app.route("/api/v1/sales/<int:id>", methods = ['GET'])
+@jwt_required
 def get_single_sales_record(id):
     sale_record = [sale for sale in sales if sale['id'] == id]
     return jsonify({'sale':sale_record[0]})
 
 
 @app.route("/api/v1/products", methods=['POST'])
+@jwt_required
 def add_new_product():
     product = {
         "id" : '1',
@@ -98,6 +79,7 @@ def add_new_product():
 
 
 @app.route("/api/v1/sales", methods=['POST'])
+@jwt_required
 def create_a_sale():
     sale = {
         "id" : 5,
@@ -112,6 +94,7 @@ def create_a_sale():
 
 
 @app.route("/api/v1/products/<int:id>", methods = ['PUT'])
+@jwt_required
 def update_product(id):
     my_product = [product for product in products if product['id'] == id]
     
@@ -123,6 +106,7 @@ def update_product(id):
 
 
 @app.route("/api/v1/products/<int:id>", methods = ['DELETE'])
+@jwt_required
 def delete_product(id):
     my_product = [product for product in products if product['id'] == id]
     products.remove(my_product[0])
